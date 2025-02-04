a7bf713e3e89f21630d6d35cb90e54e3276dad9e
zohaib521321 <khanzohaib3231@gmail.com>
2025-02-05 01:48:57 +0500

    default route set

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from app.api import login_router
 import uvicorn
 app=FastAPI()
+@app.get("/")
+def default_route():
+    return JSONResponse(
+        content={
+            "message":"Server is running"
+        }
+    )
 app.include_router(router=login_router,prefix="/login",tags=["Login"])
 if __name__=="__main__":
     uvicorn.run(app="main:app",reload=True)
